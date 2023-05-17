28e5359de5e99f177d09308b031c5f1ec03261d3
joelondev <joelonjohnson@gmail.com>
2023-05-17 07:10:17 -0700

    add file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- convert hbtn_0c_0 database to utf8mb4, utf8mb4_unicode_ci
-ALTER DATABASE hbtn_0c_0 
-CHARACTER SET utf8mb4 
-COLLATE utf8mb6_unicode_ci
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci
 
 -- Convert table first_table
 USE hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- display the avegrage temperature(Farenheit) by city ordered by temperature
+-- in descending order
+SELECT 
