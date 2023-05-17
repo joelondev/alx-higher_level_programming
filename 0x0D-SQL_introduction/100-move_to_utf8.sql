edabf1222619ed889d590c9f0562ca6e3ffbe33f
joelondev <joelonjohnson@gmail.com>
2023-05-17 13:16:42 +0000

    add file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,9 @@
 ALTER DATABASE hbtn_0c_0 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb6_unicode_ci
+
+-- Convert table first_table
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
