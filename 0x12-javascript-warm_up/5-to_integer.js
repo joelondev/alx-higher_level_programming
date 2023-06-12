821c5b509d7f8bdbfb82bb42e23c71353c2022a7
joelondev <joelonjohnson@gmail.com>
2023-06-12 15:47:20 -0700

    add file

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
+/*
+  A JS script that prints My number: <first argument converted in integer>
+  of the process.argv array.
+
+  @value: A variable holding the value of the the process.argv at index
+          2. then using the Number constructor to change the data type.
+
+*/
 const convertArg = parseInt(process.argv[2]);
 if (isNaN(convertArg)) {
     console.log('Not a number');
