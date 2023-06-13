64c55cdfd26ad805a7d25d34e67ffa38a25df485
joelondev <joelonjohnson@gmail.com>
2023-06-13 16:19:13 -0700

    add file

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-module.exports = {
-    Rectangle: function (w, h) {
-	if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	}
+module.exports = class Rectangle {
+    constructor (w, h) {
+	if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
     }
 };
